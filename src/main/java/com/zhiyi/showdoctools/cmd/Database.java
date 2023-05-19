6ec3b1bf34dbe8e426bcd1b3d6bb768b9e398f48
zhiyish <ksgmacson@gmail.com>
2023-05-19 14:44:53 +0800

    feat: add more databae to generate markdown
    - add mysql / mssql / postgresql
    - add allinone or one Page of one table

--- src/main/java/com/zhiyi/showdoctools/cmd/Database.java
+++ src/main/java/com/zhiyi/showdoctools/cmd/Database.java
@@ -37,6 +37,10 @@ public class Database implements CommandLineRunner {
     private String apitoken;
     @Value("${showdoc.catname:/}")
     private String catname;
+    @Value("${showdoc.allinone:true}")
+    private boolean allinone;
+    @Value("${showdoc.dbtype:mysql}")
+    private String dbtype;
     @Value("${spring.datasource.hikari.schema:}")
     private String schema;
 
@@ -45,25 +49,25 @@ public class Database implements CommandLineRunner {
         if (StrUtil.isEmpty(schema)) {
             throw new Exception("数据库schema不能为空！");
         }
-        String url = StrUtil.removeSuffix(serverUrl, "/");
-
-        Map<String, Object> param = new HashMap<>();
-        param.put("from", "shell");
-        param.put("api_key", apikey);
-        param.put("api_token", apitoken);
-        param.put("cat_name", catname);
-        param.put("page_title", schema);
-        param.put("page_content", getTables());
-
-        String ret = HttpUtil.post(url, param);
+        String ret = "";
+        if (allinone) {
+            ret = getAllInOne();
+        } else {
+            ret = getMultiPage();
+        }
         System.out.println(ret);
-        System.exit(0);
     }
 
+    /**
+     * 获得 单个 markdown文件，标题为数据库schema名称
+     *
+     * @return String
+     */
+    private String getAllInOne() {
+        String result = "";
 
-    private String getTables() {
         StringBuilder allTables = new StringBuilder();
-        String sqlTables = "SELECT T.TABLE_NAME as TABLE_NAME, C.COMMENTS as COMMENTS FROM USER_TABLES T, USER_TAB_COMMENTS C WHERE T.TABLE_NAME = C.TABLE_NAME ORDER BY T.TABLE_NAME";
+        String sqlTables = Tables.getSql4Table(dbtype, schema);
         List<Map<String, Object>> tableList = jdbcTemplate.queryForList(sqlTables);
         allTables.append(StrUtil.LF).append("[TOC]").append(StrUtil.LF).append(StrUtil.LF).append("** 文档更新时间：" + DateUtil.now() + " **");
         for (Map<String, Object> tableMap : tableList) {
@@ -76,32 +80,7 @@ public class Database implements CommandLineRunner {
                 String tableInfo = String.format(mdTableName, "`" + tableName + "`" + " (" + tableComment + ")");
                 log.debug("table data={}", tableInfo);
                 allTables.append(StrUtil.LF).append(tableInfo);
-                String sqlColumns = "SELECT  " +
-                        "    T.TABLE_NAME,  " +
-                        "    T.COLUMN_NAME,  " +
-                        "    T.DATA_DEFAULT,  " +
-                        "    T.NULLABLE, " +
-                        "    T.DATA_TYPE || " +
-                        "    CASE " +
-                        "        WHEN T.CHAR_LENGTH > 0  " +
-                        "        THEN '(' || T.CHAR_LENGTH || ')' " +
-                        "        WHEN T.DATA_SCALE > 0  " +
-                        "        THEN '(' || T.DATA_PRECISION || ', ' || T.DATA_SCALE || ')' " +
-                        "        WHEN T.DATA_PRECISION > 0  " +
-                        "        THEN '(' || T.DATA_PRECISION || ')' " +
-                        "        ELSE ''  " +
-                        "    END AS DATA_TYPE, " +
-                        "    C.COMMENTS " +
-                        "FROM  " +
-                        "    USER_TAB_COLUMNS  T,  " +
-                        "    USER_COL_COMMENTS C " +
-                        "WHERE  " +
-                        "    T.TABLE_NAME = C.TABLE_NAME  " +
-                        "AND T.COLUMN_NAME = C.COLUMN_NAME " +
-                        "AND T.TABLE_NAME = %s " +
-                        "ORDER BY  " +
-                        "    T.TABLE_NAME,  " +
-                        "    T.COLUMN_ID";
+                String sqlColumns = Tables.getSql4Column(dbtype, schema);
                 List<Map<String, Object>> columns = jdbcTemplate.queryForList(String.format(sqlColumns, StrUtil.wrap(tableName, "'")));
                 allTables.append(StrUtil.LF).append("|字段|类型|允许空|默认值|注释|").append(StrUtil.LF).append("|:----|:----|:----|----|----|");
                 for (Map<String, Object> column : columns) {
@@ -114,19 +93,105 @@ public class Database implements CommandLineRunner {
                     String COMMENTS = MapUtil.getStr(column, "COMMENTS", "字段无备注");
                     String colNewComment = StrUtil.removeAllLineBreaks(COMMENTS);
                     String colNewDATA_DEFAULT = StrUtil.removeAllLineBreaks(DATA_DEFAULT);
-                    if ((COMMENTS.contains(StrUtil.LF)) || (COMMENTS.contains(StrUtil.CRLF)) || COMMENTS.contains(" ")) {
+                   /*
+
+                   if ((COMMENTS.contains(StrUtil.LF)) || (COMMENTS.contains(StrUtil.CRLF)) || COMMENTS.contains(" ")) {
+                        // 备注：清理多余空格与换行符
                         jdbcTemplate.execute("COMMENT ON column " + tableName + "." + COLUMN_NAME + " IS " + " '" + StrUtil.removeAny(colNewComment, " ") + "'");
                     }
                     if ((DATA_DEFAULT.contains(StrUtil.LF)) || (DATA_DEFAULT.contains(StrUtil.CRLF)) || DATA_DEFAULT.contains(" ")) {
+                        // 默认值：清理多余空格与换行符
                         jdbcTemplate.execute("ALTER TABLE " + tableName + " MODIFY (" + COLUMN_NAME + " DEFAULT " + StrUtil.removeAny(colNewDATA_DEFAULT, " ") + ")");
                     }
+                    */
                     String columnInfo = String.format(mdColumns, COLUMN_NAME, DATA_TYPE, NULLABLE, colNewDATA_DEFAULT, colNewComment);
                     log.debug("column data={}", columnInfo);
                     allTables.append(StrUtil.LF).append(columnInfo);
                 }
             }
         }
-        return allTables.toString();
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("from", "shell");
+        param.put("api_key", apikey);
+        param.put("api_token", apitoken);
+        param.put("cat_name", catname);
+        param.put("page_title", schema);
+        param.put("page_content", allTables.toString());
+
+        result = HttpUtil.post(serverUrl, param);
+        return result;
     }
+
+    /**
+     * 获得 多个个 markdown文件，标题为表名称
+     *
+     * @return String
+     */
+    private String getMultiPage() {
+        String result = "共%d 张表，完成 %d ";
+
+        int i = 0;
+
+        String sqlTables = Tables.getSql4Table(dbtype, schema);
+        List<Map<String, Object>> tableList = jdbcTemplate.queryForList(sqlTables);
+        for (Map<String, Object> tableMap : tableList) {
+            StringBuilder allTables = new StringBuilder();
+            allTables.append(StrUtil.LF).append(StrUtil.LF).append("** 文档更新时间：" + DateUtil.now() + " **");
+
+            String mdTableName = "## %s";
+
+            String tableName = MapUtil.getStr(tableMap, "TABLE_NAME");
+            String tableComment = MapUtil.getStr(tableMap, "COMMENTS", "本表无说明");
+
+            if (!Tables.excludeWithStr(tableName, ignorePre, ignoreEnd)) {
+                String tableInfo = String.format(mdTableName, "`" + tableName + "`" + " (" + tableComment + ")");
+                log.debug("table data={}", tableInfo);
+                allTables.append(StrUtil.LF).append(tableInfo);
+                String sqlColumns = Tables.getSql4Column(dbtype, schema);
+                List<Map<String, Object>> columns = jdbcTemplate.queryForList(String.format(sqlColumns, StrUtil.wrap(tableName, "'")));
+                allTables.append(StrUtil.LF).append("|字段|类型|允许空|默认值|注释|").append(StrUtil.LF).append("|:----|:----|:----|----|----|");
+                for (Map<String, Object> column : columns) {
+                    String mdColumns = "|%s|%s|%s|%s|%s|";
+
+                    String COLUMN_NAME = MapUtil.getStr(column, "COLUMN_NAME");
+                    String DATA_TYPE = MapUtil.getStr(column, "DATA_TYPE", "");
+                    String NULLABLE = MapUtil.getStr(column, "NULLABLE", "");
+                    String DATA_DEFAULT = MapUtil.getStr(column, "DATA_DEFAULT", "");
+                    String COMMENTS = MapUtil.getStr(column, "COMMENTS", "字段无备注");
+                    String colNewComment = StrUtil.removeAllLineBreaks(COMMENTS);
+                    String colNewDATA_DEFAULT = StrUtil.removeAllLineBreaks(DATA_DEFAULT);
+                   /*
+                   if ((COMMENTS.contains(StrUtil.LF)) || (COMMENTS.contains(StrUtil.CRLF)) || COMMENTS.contains(" ")) {
+                        // 备注：清理多余空格与换行符
+                        jdbcTemplate.execute("COMMENT ON column " + tableName + "." + COLUMN_NAME + " IS " + " '" + StrUtil.removeAny(colNewComment, " ") + "'");
+                    }
+                    if ((DATA_DEFAULT.contains(StrUtil.LF)) || (DATA_DEFAULT.contains(StrUtil.CRLF)) || DATA_DEFAULT.contains(" ")) {
+                        // 默认值：清理多余空格与换行符
+                        jdbcTemplate.execute("ALTER TABLE " + tableName + " MODIFY (" + COLUMN_NAME + " DEFAULT " + StrUtil.removeAny(colNewDATA_DEFAULT, " ") + ")");
+                    }
+                    */
+                    String columnInfo = String.format(mdColumns, COLUMN_NAME, DATA_TYPE, NULLABLE, colNewDATA_DEFAULT, colNewComment);
+                    log.debug("column data={}", columnInfo);
+                    allTables.append(StrUtil.LF).append(columnInfo);
+                }
+            }
+            Map<String, Object> param = new HashMap<>();
+            param.put("from", "shell");
+            param.put("api_key", apikey);
+            param.put("api_token", apitoken);
+            param.put("cat_name", catname);
+            param.put("page_title", tableName);
+            param.put("page_content", allTables.toString());
+            try {
+                result = HttpUtil.post(serverUrl, param);
+                i = i + 1;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return String.format(result, tableList.size(), i);
+    }
+
 
 }
--- src/main/java/com/zhiyi/showdoctools/template/Tables.java
+++ src/main/java/com/zhiyi/showdoctools/template/Tables.java
@@ -28,4 +28,110 @@ public class Tables {
 
         return result;
     }
+
+    public static String getSql4Table(String dbtype, String schema) {
+        String result = "";
+        if ("oracle".equalsIgnoreCase(dbtype)) {
+            result = "SELECT T.TABLE_NAME as TABLE_NAME, C.COMMENTS as COMMENTS " +
+                    " FROM USER_TABLES T, USER_TAB_COMMENTS C " +
+                    " WHERE T.TABLE_NAME = C.TABLE_NAME " +
+                    " ORDER BY T.TABLE_NAME";
+        } else if ("postgresql".equalsIgnoreCase(dbtype)) {
+            result = "SELECT T.table_name as TABLE_NAME, COALESCE(obj_description(relfilenode, 'pg_class'), '') AS COMMENTS  " +
+                    "  FROM information_schema.TABLES AS T, pg_class C  " +
+                    "  WHERE T.table_name = C.relname  " +
+                    "  AND TABLE_SCHEMA =  " + StrUtil.wrap(schema, "'") +
+                    "  ORDER BY T.table_name";
+        } else if ("sqlserver".equalsIgnoreCase(dbtype)) {
+            result = "SELECT TABLE_NAME " +
+                    " FROM INFORMATION_SCHEMA.TABLES " +
+                    " WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = " + StrUtil.wrap(schema, "'") +
+                    " ORDER BY TABLE_NAME ";
+        } else {
+            result = "SELECT TABLE_NAME, TABLE_COMMENT " +
+                    " FROM information_schema.TABLES " +
+                    " WHERE TABLE_SCHEMA = " + StrUtil.wrap(schema, "'");
+        }
+
+        return result;
+    }
+
+    public static String getSql4Column(String dbtype, String schema) {
+        String result = "";
+        if ("oracle".equalsIgnoreCase(dbtype)) {
+            result = "SELECT  " +
+                    "    T.TABLE_NAME,  " +
+                    "    T.COLUMN_NAME,  " +
+                    "    T.DATA_DEFAULT,  " +
+                    "    T.NULLABLE, " +
+                    "    T.DATA_TYPE || " +
+                    "    CASE " +
+                    "        WHEN T.CHAR_LENGTH > 0  " +
+                    "        THEN '(' || T.CHAR_LENGTH || ')' " +
+                    "        WHEN T.DATA_SCALE > 0  " +
+                    "        THEN '(' || T.DATA_PRECISION || ', ' || T.DATA_SCALE || ')' " +
+                    "        WHEN T.DATA_PRECISION > 0  " +
+                    "        THEN '(' || T.DATA_PRECISION || ')' " +
+                    "        ELSE ''  " +
+                    "    END AS DATA_TYPE, " +
+                    "    C.COMMENTS " +
+                    "FROM  " +
+                    "    USER_TAB_COLUMNS  T,  " +
+                    "    USER_COL_COMMENTS C " +
+                    "WHERE  " +
+                    "    T.TABLE_NAME = C.TABLE_NAME  " +
+                    "AND T.COLUMN_NAME = C.COLUMN_NAME " +
+                    "AND T.TABLE_NAME = %s " +
+                    "ORDER BY  " +
+                    "    T.TABLE_NAME,  " +
+                    "    T.COLUMN_ID";
+        } else if ("postgresql".equalsIgnoreCase(dbtype)) {
+            result = "SELECT COL.table_name as TABLE_NAME," +
+                    " COL.column_name as COLUMN_NAME," +
+                    " COL.column_default as DATA_DEFAULT," +
+                    " COL.is_nullable as NULLABLE," +
+                    " COL.udt_name as DATA_TYPE, " +
+                    "  COALESCE(col_description(ATT.attrelid, ATT.attnum), '') AS COMMENTS " +
+                    "  FROM information_schema.COLUMNS COL, pg_attribute ATT, pg_class CLS " +
+                    "  WHERE COL.TABLE_SCHEMA = " + StrUtil.wrap(schema, "'") +
+                    "  AND COL.column_name = ATT.attname AND COL.table_name = CLS.relname " +
+                    " AND ATT.attrelid = CLS.oid " +
+                    " AND TABLE_NAME=%s " +
+                    " ORDER BY COL.table_name, COL.ordinal_position";
+        } else if ("sqlserver".equalsIgnoreCase(dbtype)) {
+            result = "SELECT C.TABLE_NAME," +
+                    " C.COLUMN_NAME," +
+                    " C.COLUMN_DEFAULT AS DATA_DEFAULT," +
+                    " C.IS_NULLABLE AS NULLABLE,  " +
+                    "    C.DATA_TYPE +  " +
+                    "     CASE  " +
+                    "     WHEN C.CHARACTER_MAXIMUM_LENGTH IS NOT NULL THEN '('+ CONVERT(VARCHAR(10), C.CHARACTER_MAXIMUM_LENGTH) +')'  " +
+                    "     WHEN C.NUMERIC_SCALE > 0 THEN '('+ CONVERT(VARCHAR(10), C.NUMERIC_PRECISION) +', ' + CONVERT(VARCHAR(10), C.NUMERIC_SCALE) +')' " +
+                    "     WHEN C.NUMERIC_PRECISION > 0 THEN '('+ CONVERT(VARCHAR(10), C.NUMERIC_PRECISION) +')' " +
+                    "     ELSE '' END  " +
+                    "    AS DATA_TYPE, " +
+                    "    ISNULL(EP.COLUMN_COMMENT, '') AS COMMENTS " +
+                    "   FROM INFORMATION_SCHEMA.COLUMNS AS C " +
+                    "   LEFT JOIN ( " +
+                    "    SELECT TB.name AS TABLE_NAME, COL.name AS COLUMN_NAME, EP.value AS COLUMN_COMMENT " +
+                    "    FROM sys.extended_properties AS EP, sys.tables AS TB, sys.columns AS COL " +
+                    "    WHERE EP.major_id = TB.object_id AND EP.minor_id = COL.column_id AND TB.object_id = COL.object_id " +
+                    "   ) AS EP ON EP.TABLE_NAME = C.TABLE_NAME AND EP.COLUMN_NAME = C.COLUMN_NAME " +
+                    "   WHERE TABLE_CATALOG = " + StrUtil.wrap(schema, "'") +
+                    " AND TABLE_NAME=%s " +
+                    "   ORDER BY TABLE_NAME, ORDINAL_POSITION";
+        } else {
+            result = "SELECT TABLE_NAME, " +
+                    " COLUMN_NAME, " +
+                    " COLUMN_DEFAULT AS DATA_DEFAULT, " +
+                    " IS_NULLABLE AS NULLABLE, " +
+                    " COLUMN_TYPE AS DATA_TYPE, " +
+                    " COLUMN_COMMENT AS  AS COMMENTS " +
+                    " FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = " + StrUtil.wrap(schema, "'") +
+                    " AND TABLE_NAME=%s " +
+                    " ORDER BY ORDINAL_POSITION ";
+        }
+
+        return result;
+    }
 }
